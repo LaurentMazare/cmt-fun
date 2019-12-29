ee80f7522ef22cb9a810083ab981708009a21d7c
Laurent <lmazare@gmail.com>
2019-12-29 16:02:43 +0000

    More cmt fun.

--- examples/cmt_test.ml
+++ examples/cmt_test.ml
@@ -1,22 +1,33 @@
-open! Base;;
+open! Base
 
-let print_sign : Types.signature_item -> unit = function
-  | Sig_value (ident, _, _) -> Stdio.printf "  value %s\n%!" (Ident.name ident)
-  | Sig_type (ident, _, _, _) -> Stdio.printf "  type %s\n%!" (Ident.name ident)
-  | Sig_typext (ident, _, _, _) -> Stdio.printf "  typext %s\n%!" (Ident.name ident)
-  | Sig_module (ident, _, _, _, _) -> Stdio.printf "  module %s\n%!" (Ident.name ident)
-  | Sig_modtype (ident, _, _) -> Stdio.printf "  modtype %s\n%!" (Ident.name ident)
-  | Sig_class (ident, _, _, _) -> Stdio.printf "  class %s\n%!" (Ident.name ident)
-  | Sig_class_type (ident, _, _, _) -> Stdio.printf "  classtype %s\n%!" (Ident.name ident)
+let rec print_sign : string -> Types.signature_item -> unit =
+ fun indent s ->
+  match s with
+  | Sig_value (ident, _, _) -> Stdio.printf "%svalue %s\n%!" indent (Ident.name ident)
+  | Sig_type (ident, _, _, _) -> Stdio.printf "%stype %s\n%!" indent (Ident.name ident)
+  | Sig_typext (ident, _, _, _) ->
+    Stdio.printf "%stypext %s\n%!" indent (Ident.name ident)
+  | Sig_module (ident, _, module_declaration, _, _) ->
+    Stdio.printf "%smodule %s\n%!" indent (Ident.name ident);
+    (match module_declaration.md_type with
+    | Mty_ident path -> Stdio.printf "%s  ident %s\n%!" indent (Path.name path)
+    | Mty_signature signature ->
+      Stdio.printf "%ssig\n%!" indent;
+      List.iter signature ~f:(print_sign (indent ^ "  "));
+      Stdio.printf "%ssigend\n%!" indent
+    | Mty_functor (_, _, _) -> Stdio.printf "      functor\n%!"
+    | Mty_alias _ -> Stdio.printf "%s  alias\n%!" indent)
+  | Sig_modtype (ident, _, _) -> Stdio.printf "%smodtype %s\n%!" indent (Ident.name ident)
+  | Sig_class (ident, _, _, _) -> Stdio.printf "%sclass %s\n%!" indent (Ident.name ident)
+  | Sig_class_type (ident, _, _, _) ->
+    Stdio.printf "%sclasstype %s\n%!" indent (Ident.name ident)
 
 let () =
   let cmi_infos, cmt_infos = Cmt_format.read "core__Core_date.cmti" in
   Stdio.printf "cmt loaded\n%!";
   Option.iter cmi_infos ~f:(fun cmi_infos ->
-    Stdio.printf "cmi_name %s\n%!" cmi_infos.cmi_name;
-    Stdio.printf "cmi_sign %d\n%!" (List.length cmi_infos.cmi_sign);
-    List.iter cmi_infos.cmi_sign ~f:print_sign;
-  );
+      Stdio.printf "cmi_name %s\n%!" cmi_infos.cmi_name;
+      Stdio.printf "cmi_sign %d\n%!" (List.length cmi_infos.cmi_sign);
+      List.iter cmi_infos.cmi_sign ~f:(print_sign "  "));
   Option.iter cmt_infos ~f:(fun cmt_infos ->
-    Stdio.printf "cmt_name %s\n%!" cmt_infos.cmt_modname)
-;;
+      Stdio.printf "cmt_name %s\n%!" cmt_infos.cmt_modname)
